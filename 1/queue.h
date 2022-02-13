6254842fe336fe001be0100c6402a0e7630e9ab5
BenasB <29711974+BenasB@users.noreply.github.com>
2022-02-13 11:46:44 +0200

    Improved header

--- 1/queue.h
+++ 1/queue.h
@@ -3,12 +3,27 @@
 
 #include <stdbool.h>
 
-void queue_new();
-void queue_delete();
-void enqueue();
-void dequeue();
-unsigned int size();
-bool isEmpty();
-void peek();
+typedef struct QueueNode QueueNode;
+typedef struct Queue Queue;
+
+struct QueueNode
+{
+    void *data; // For generic data
+    QueueNode *next;
+};
+
+struct Queue
+{
+    QueueNode *head;
+    QueueNode *tail;
+};
+
+Queue *queue_new();
+bool isEmpty(Queue queue);
+void enqueue(void *data);
+void dequeue(void *data);
+void peek(Queue queue);
+unsigned int size(Queue queue);
+void queue_delete(Queue queue);
 
 #endif
