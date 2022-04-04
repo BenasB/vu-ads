6d630dce62240776b99f46bcd6c053947e0cf832
BenasB <29711974+BenasB@users.noreply.github.com>
2022-04-04 22:58:49 +0300

    LFF: Programming
    <START=22:30>Changed knight move combination to for loops and getLowestAttackabilityTile

--- 2/main.c
+++ 2/main.c
@@ -3,7 +3,17 @@
 #include <string.h>
 
 #define BOARD_SIZE 8
+#define EMPTY_TILE 0
+#define ATTACKED_TILE 1
+#define KNIGHT_TILE 2
 typedef char Board[BOARD_SIZE][BOARD_SIZE];
+
+struct Tile
+{
+    int i, j;
+};
+
+typedef struct Tile Tile;
 
 bool inBoard(int i, int j){
     return (i >= 0 && i < BOARD_SIZE) && (j >= 0 && j < BOARD_SIZE);
@@ -25,16 +35,37 @@ void generateAttackMap(Board *board){
     {
         for (int j = 0; j < BOARD_SIZE; j++)
         {
-            if (inBoard(i-2, j-1)) (*board)[i-2][j-1] += 1;
-            if (inBoard(i-2, j+1)) (*board)[i-2][j+1] += 1;
-            if (inBoard(i-1, j+2)) (*board)[i-1][j+2] += 1;
-            if (inBoard(i+1, j+2)) (*board)[i+1][j+2] += 1;
-            if (inBoard(i+2, j+1)) (*board)[i+2][j+1] += 1;
-            if (inBoard(i+2, j-1)) (*board)[i+2][j-1] += 1;
-            if (inBoard(i+1, j-2)) (*board)[i+1][j-2] += 1;
-            if (inBoard(i-1, j-2)) (*board)[i-1][j-2] += 1;
+            for(int dx = -2; dx <= 2; dx++)
+            {
+                for(int dy = -2; dy <= 2; dy++)
+                {
+                    if((dx*dx + dy*dy) == 5) // Defined by the way knights move
+                    {
+                        if (inBoard(i+dx, j+dy)) (*board)[i+dx][j+dy] += 1;
+                    }
+                }
+            }
         }
     }
+}
+
+Tile getLowestAttackabilityTile(Board board, Board attackMap){
+    Tile tile;
+    int lowest = 0xFFFF;
+    for (int i = 0; i < BOARD_SIZE; i++)
+    {
+        for (int j = 0; j < BOARD_SIZE; j++)
+        {
+            if (board[i][j] == EMPTY_TILE && attackMap[i][j] < lowest)
+            {
+                lowest = attackMap[i][j];
+                tile.i = i;
+                tile.j = j;
+            }
+        }
+    }
+
+    return tile;
 }
 
 int main(){
@@ -42,5 +73,9 @@ int main(){
     memset(attackMap, 0, BOARD_SIZE*BOARD_SIZE*sizeof(char));
     generateAttackMap(&attackMap);
     printBoard(attackMap);
-    printf("Hello world!\n");
+    Board board;
+    memset(board, 0, BOARD_SIZE*BOARD_SIZE*sizeof(char));
+    Tile lowestTile = getLowestAttackabilityTile(board, attackMap);
+
+    printf("Lowest i: %d j:%d\n", lowestTile.i, lowestTile.j);
 }
